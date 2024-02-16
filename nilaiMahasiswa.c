7514a39ad6bf444bf4832543059a0359a4484772
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2024-02-16 21:47:38 +0700

    menambahkan fitur grade nilai pada file nilaiMahasiswa.c

--- nilaiMahasiswa.c
+++ nilaiMahasiswa.c
@@ -15,5 +15,31 @@ int main()
         printf("%-20s%d: ", "Nilai mahasiswa ke-", i + 1);
         scanf("%d", &nilai[i]);
     }
+
+    putchar('\n');
+
+    for (int i = 0; i < jmlMahasiswa; i++)
+    {
+        if (nilai[i] >= 85)
+        {
+            printf("Mahasiswa ke-%d: grade A\n", i + 1);
+        }
+        else if (nilai[i] >= 75)
+        {
+            printf("Mahasiswa ke-%d: grade B\n", i + i);
+        }
+        else if (nilai[i] >= 65)
+        {
+            printf("Mahasiswa ke-%d: grade C\n", i + 1);
+        }
+        else if (nilai[i] >= 55)
+        {
+            printf("Mahasiswa ke-%d: grade D\n", i + 1);
+        }
+        else
+        {
+            printf("Mahasiswa ke-%d: grade E\n", i + 1);
+        }
+    }
     return 0;
 }
