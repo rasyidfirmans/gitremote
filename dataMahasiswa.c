ff692d37bd521878e14d158b975024e049a9c762
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2024-02-16 21:06:48 +0700

    change: modifikasi file dataMahasiswa.c

--- dataMahasiswa.c
+++ dataMahasiswa.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+typedef struct dataMahasiswa
 {
     char nama[101];
     int nilai;
+} dataMahasiswa;
 
-    printf("%-7s: ", "Nama");
-    fgets(nama, sizeof(nama), stdin);
+int main()
+{
+    int jmlMahasiswa;
+    printf("Banyak mahasiswa: ");
+    scanf("%d", &jmlMahasiswa);
+    getchar();
 
-    printf("%-7s: ", "Nilai");
-    scanf("%d", &nilai);
+    dataMahasiswa *mahasiswa;
+    mahasiswa = (dataMahasiswa *)malloc(jmlMahasiswa * sizeof(dataMahasiswa));
+
+    for (int i = 0; i < jmlMahasiswa; i++)
+    {
+        printf("%-7s: ", "Nama");
+        fgets(mahasiswa->nama, sizeof(mahasiswa->nama), stdin);
+
+        printf("%-7s: ", "Nilai");
+        scanf("%d", &mahasiswa->nilai);
+        getchar();
+
+        putchar('\n');
+    }
 }
