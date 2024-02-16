9ff439764eabf496aed30e2d7294b4e029afa659
Rasyid Firmansyah <rasyidfirmansyah56@student.uns.ac.id>
2024-02-16 21:37:05 +0700

    menambahkan batasan nilai pada file nilaiMahasiswa.c

--- nilaiMahasiswa.c
+++ nilaiMahasiswa.c
@@ -15,5 +15,19 @@ int main()
         printf("%-20s%d: ", "Nilai mahasiswa ke-", i + 1);
         scanf("%d", &nilai[i]);
     }
+
+    putchar('\n');
+
+    for (int i = 0; i < jmlMahasiswa; i++)
+    {
+        if (nilai[i] < 65)
+        {
+            printf("Mahasiswa ke-%d: Tidak lulus\n", i + 1);
+        }
+        else
+        {
+            printf("Mahasiswa ke-%d: Lulus\n", i + 1);
+        }
+    }
     return 0;
 }
